ae1e838249ab8da7e60d363c3387ed896a013407
Pradeeptalari14 <talaripradeep45@gmail.com>
2026-05-30 01:19:51 +0530

    feat: migrate to modular Vite/HTML project and setup automated GitHub Pages pipeline

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [injectHTML()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tools: resolve(__dirname, 'tools/index.html'),
+        ansible: resolve(__dirname, 'tools/an/index.html'),
+        docker: resolve(__dirname, 'tools/do/index.html'),
+        jenkins: resolve(__dirname, 'tools/je/index.html'),
+        terraform: resolve(__dirname, 'tools/te/index.html'),
+      }
+    }
+  }
+});
