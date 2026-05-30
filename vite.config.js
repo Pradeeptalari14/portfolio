92c7788b31dfcf2c598442a7fd2f65739f66da50
Pradeeptalari14 <talaripradeep45@gmail.com>
2026-05-30 20:12:28 +0530

    refactor(tools): rename subdirectories to full names and update configurations

--- vite.config.js
+++ vite.config.js
@@ -9,16 +9,16 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         tools: resolve(__dirname, 'tools/index.html'),
-        ansible: resolve(__dirname, 'tools/an/index.html'),
-        docker: resolve(__dirname, 'tools/do/index.html'),
-        jenkins: resolve(__dirname, 'tools/je/index.html'),
-        terraform: resolve(__dirname, 'tools/te/index.html'),
-        k8s: resolve(__dirname, 'tools/k8s/index.html'),
-        mon: resolve(__dirname, 'tools/mon/index.html'),
-        github: resolve(__dirname, 'tools/gh/index.html'),
+        ansible: resolve(__dirname, 'tools/ansible/index.html'),
+        docker: resolve(__dirname, 'tools/docker/index.html'),
+        jenkins: resolve(__dirname, 'tools/jenkins/index.html'),
+        terraform: resolve(__dirname, 'tools/terraform/index.html'),
+        k8s: resolve(__dirname, 'tools/kubernetes/index.html'),
+        mon: resolve(__dirname, 'tools/monitoring/index.html'),
+        github: resolve(__dirname, 'tools/github-actions/index.html'),
         helm: resolve(__dirname, 'tools/helm/index.html'),
-        argo: resolve(__dirname, 'tools/argo/index.html'),
-        logging: resolve(__dirname, 'tools/log/index.html'),
+        argo: resolve(__dirname, 'tools/argocd/index.html'),
+        logging: resolve(__dirname, 'tools/logging/index.html'),
       }
     }
   }
